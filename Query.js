a42c5a349476097cb08b5ad8d92a4f894cb05d28
Alshahriar78 <alshahriar78@gmail.com>
2023-10-01 13:13:48 -0400

    changes

--- Query.js
+++ Query.js
@@ -1,12 +1,20 @@
-let http = require('http');
-let MongoClient = require('mongodb').MongoClient;
-let url = "mongodb://localhost:27017/CraftShop";
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    let query = { City: "Sylhet" };
-    db.collection("employees").find(query).toArray(function(err, result) {
-        if (err) throw err;
-        console.log(result);
-        db.close();
-    });
-});
+user('DataBaseName')
+db.createCollection('NewCollection');//Create a NewCollection in DataBaseName  
+
+user('DataBaseName')
+//Inserts a single document into a collection.
+db.NewCollection.insertOne({
+    "Name":"Al Shahoriar",  
+    "age":24,
+    "Profession":"Student",
+    "City":"Bogura",
+})
+
+user('DataBaseName')
+// Delete/remove a single document from the collection.
+db.NewCollection.deleteOne({
+   "Name":"Al Shahoriar"
+})
+
+user('DataBaseName')
+db.NewCollection.drop(); //Removes the collection from the DataBaseName in  database.
